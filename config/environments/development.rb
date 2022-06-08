6767d00bfcd410fe55c9fdf948248e0dc713af09
ksyrba <ksyrbaba0812@gmail.com>
2022-06-08 01:45:46 +0000

    webページの作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "7bf0bceec36d4f3db7d31a198d4dd2cd.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
