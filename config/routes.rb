3963f258980d8ffd8d25af43d89d89abb1ed5d5d
ksyrba <ksyrbaba0812@gmail.com>
2022-06-10 10:42:17 +0000

    パスを用いたルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'boards' => 'boards#index'
-  get 'boards/new' => 'boards#new'
-  post 'boards' => 'boards#create'
-  get 'boards/:id' => 'boards#show'
+  resources :boards, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
