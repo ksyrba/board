1958f43c38173e62dc96acb232af49274b7ef3f1
ksyrba <ksyrbaba0812@gmail.com>
2022-06-13 03:28:04 +0000

    バリデーションの表示

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,14 +1,19 @@
 class BoardsController < ApplicationController
   before_action :set_target_board, only:[:show, :edit, :update, :destroy]
-  
+
   def new
-    @board = Article.new
+    @board = Article.new(flash[:board])
   end
 
   def create
     board = Article.create(board_params)
-    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
-    redirect_to board_path(board)
+    if board.save
+      flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+      redirect_to board_path(board)
+    else
+      @board = Article.new(flash[:board])
+      render 'new'
+    end
   end
 
   def index
@@ -25,7 +30,7 @@ class BoardsController < ApplicationController
     @board.update(board_params)
     redirect_to board_path(@board)
   end
-  
+
   def destroy
     @board.destroy
     redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
@@ -35,7 +40,7 @@ class BoardsController < ApplicationController
   def board_params
     params.require(:article).permit(:name, :title, :body)
   end
-  
+
   def set_target_board
     @board = Article.find(params[:id])
   end
