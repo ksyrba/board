a9cbe7f550e2fe264f3a6fe8a6516648cb7ae91c
ksyrba <ksyrbaba0812@gmail.com>
2022-06-13 04:52:40 +0000

    アソシエーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,8 @@
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
+  
+  
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { maximum: 1000 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_025021) do
+ActiveRecord::Schema.define(version: 2022_06_13_044822) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -20,4 +20,14 @@ ActiveRecord::Schema.define(version: 2022_06_08_025021) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+  end
+
+  add_foreign_key "comments", "boards"
 end
