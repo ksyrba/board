28ea3cd69b7e54d2f1c15823bcb37508a05badfe
ksyrba <ksyrbaba0812@gmail.com>
2022-06-14 11:51:54 +0000

    アソシエーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,6 +11,8 @@
 #
 class Article < ApplicationRecord
   has_many :comments
+  has_many :board_tag_relations
+  has_many :tags, through: :board_tag_relations
 
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_13_044822) do
+ActiveRecord::Schema.define(version: 2022_06_14_112742) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2022_06_13_044822) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "board_tag_relations", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_board_tag_relations_on_board_id"
+    t.index ["tag_id"], name: "index_board_tag_relations_on_tag_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -29,5 +38,13 @@ ActiveRecord::Schema.define(version: 2022_06_13_044822) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "board_tag_relations", "boards"
+  add_foreign_key "board_tag_relations", "tags"
   add_foreign_key "comments", "articles"
 end
