708bb818f112e2d8f1992415a5dfd8ef58f0b62c
ksyrba <ksyrbaba0812@gmail.com>
2022-06-15 04:36:55 +0000

    ユーザー認証の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'boards#index'
+  root to: 'home#index'
   resources :boards
   resources :comments, only:[:create, :destroy]
+  resources :users, only:[:new, :create]
+  get 'mypage' => 'users#me'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_112742) do
+ActiveRecord::Schema.define(version: 2022_06_15_042118) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2022_06_14_112742) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "board_tag_relations", "articles"
